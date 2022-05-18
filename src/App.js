c8ba9a4ce85a2dcf7ebcd61d77402f9e6198df17
jiyuuki <salma.hajrawi@gmail.com>
2022-05-18 15:46:56 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import Header from './components/Header/Header';
 import Board from './pages/Board/Board';
 import Backlog from './pages/Backlog/Backlog';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <Board />
-      <Backlog />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Board />} />
+          <Route path="/backlog" element={<Backlog />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
